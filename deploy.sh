233b6b0ac85e806f13299c81db9d862562251bda
Nguyễn Ngọc <nguyenngoc@Nguyens-MacBook-Pro.local>
2020-10-08 21:19:27 +0900

    Deploy test

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:egd-ngocnd/vuejs-demo.git master:gh-pages
 
 cd -
