04f81007282a47a085d8497bda79309c33b26e5a
egd-ngocnd <ngocnd@egdgroup.com>
2020-10-09 06:18:05 +0900

    small fix

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import Sorting2 from "./components/Sorting2";
 import HelloWorld from "./components/HelloWorld";
 
 export const routes = [
-    { path: "", component: Sorting },
+    { path: "/", component: Sorting },
+    { path: "/vuejs-demo", component: Sorting },
     { path: "/menu2", component: Sorting2},
     { path: "/menu3", component: HelloWorld}
 ]
