88b4629eb1e6a78987aee89e99b34691dd0c1f59
hbl-ngocnd <nguyen_duc_ngoc@abema.tv>
2022-07-17 18:21:16 +0700

    feat:convert to typescript

---
+++ src/components/AppProvider.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-loading-bar-provider>
+    <n-message-provider>
+      <n-dialog-provider>
+        <slot></slot>
+      </n-dialog-provider>
+    </n-message-provider>
+  </n-loading-bar-provider>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+  NMessageProvider,
+  NLoadingBarProvider,
+  NDialogProvider,
+} from 'naive-ui';
+
+export default defineComponent({
+  name: 'AppProvider',
+  components: {
+    NMessageProvider,
+    NLoadingBarProvider,
+    NDialogProvider,
+  },
+});
+</script>
---
+++ src/views/SortingView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Sorting from '@/components/Sorting.vue'
+</script>
+
+<template>
+  <main>
+    <Sorting />
+  </main>
+</template>
